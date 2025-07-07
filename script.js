a21900d7f4a0d4f759331ce6a32350b2b8faddb1
KindOfTired <140853735+KindOfTired@users.noreply.github.com>
2025-07-07 23:33:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ function enterSite() {
   document.querySelector('.intro').style.display = 'none';
   document.querySelector('.content').classList.remove('hidden');
 
+  const audio = document.getElementById("bgmusic");
+  if (audio) audio.play();
+
   let count = localStorage.getItem('visits') || 0;
   count++;
   localStorage.setItem('visits', count);
